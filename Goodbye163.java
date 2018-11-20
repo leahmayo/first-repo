75471b2152a748b9972968a3454eff6dc8c69b62
Leah Mayo <lamayo@aggies.ncat.edu>
2018-11-20 12:41:32 -0500

    Goodbye

---
+++ Goodbye163.java
@@ -0,0 +1,9 @@
+/**
+ *Simple class to print a goodbye message to the beloved class COMP163.
+ *@author lamayo@aggies.ncat.edu
+ */
+public class Goodbye163 {
+  public static void main(String[] args) {
+    System.out.println("Goodbye COMP 163");
+  }
+}
